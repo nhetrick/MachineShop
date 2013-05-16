42dde0169bf350fafbfa28cd039cee7b3b5e89d6
Shawn Toffel <shawn.toffel@gmail.com>
2013-05-16 13:07:37 -0600

    moved documents into doc dir

---
+++ src/main/InputReaderException.java
@@ -0,0 +1,13 @@
+package main;
+
+public class InputReaderException extends Exception{
+
+    public InputReaderException(String message) {
+        super(message);
+    }
+
+    public InputReaderException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
+}
