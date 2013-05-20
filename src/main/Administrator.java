328cb331182a2abd7f0714087abdada5e45cd83e
shawntoffel <shawn.toffel@gmail.com>
2013-05-20 13:37:18 -0600

    Added admin levels

--- src/main/Administrator.java
+++ src/main/Administrator.java
@@ -1,50 +1,14 @@
 package main;
-import java.util.ArrayList;
-
 
 public class Administrator extends User {
 
 	public Administrator(String firstName, String lastName, int CWID) {
 		super(firstName, lastName, CWID);
+		// TODO Auto-generated constructor stub
 	}
 	
-	public void addPermission(User user, Machine machine) {
+	public void generateReport(){
 		
 	}
-	
-	public void removeUsers(ArrayList<User> users) {
-		
-	}
-	
-	public void removePermission(User user, Machine machine) {
-		
-	}
-	
-	public void generateReport() {
-		
-	}
-	
-	public void addTool(Tool t) {
-		
-	}
-	
-	public void addMachine(Machine m) {
-		
-	}
-	
-	public void removeTool(Tool t) {
-		
-	}
-	
-	public void removeMachine(Machine m) {
-		
-	}
-	
-	public void lockUser(User user) {
-		
-	}
-	
-	public void unlockUser(User user) {
-		
-	}
+
 }
