f188e20a6ca1ae0d824f91cda6d6707d6c0ce3ab
chpark <wogk12@gmail.com>
2013-06-07 14:12:03 -0600

    now you can log in as system admin

--- src/main/Administrator.java
+++ src/main/Administrator.java
@@ -2,8 +2,6 @@ package main;
 
 public class Administrator extends User {
 	
-	protected boolean isSystemAdmin = false;
-	
 	public Administrator(String firstName, String lastName, String cwid, String email, String department) {
 		super(firstName, lastName, cwid, email, department);
 		isAdmin = true;
