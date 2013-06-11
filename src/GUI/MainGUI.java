fb0c726ac7a5a0b1cf6e0ba25d9e3c7cf93ba180
Taylor Sallee <tsallee@mymail.mines.edu>
2013-06-11 09:06:39 -0600

    Fixed images

--- src/GUI/MainGUI.java
+++ src/GUI/MainGUI.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
@@ -23,104 +24,105 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.border.EmptyBorder;
 
 import main.*;
 
 public class MainGUI extends JFrame {
-	
+
 	private KeyListener reader; 
 	private AccessTracker tracker;
 	private User currentUser;
-	private JPanel centerPanel;
+
+	private JPanel mainPanel; // Basically The Entire Panel
 	private JPanel headerBar;
 	private JPanel footerBar;
-	private JPanel homeCenterPanel;
+	private JPanel mainContentPanel; // The content Panel (between header and footer)
+
 	private Clock time;
 	private Font headerFont;
+
 	private GridBagConstraints c = new GridBagConstraints();
 	private static int MAX_ERROR_COUNT = 3;
 	private JButton backButton;
 	private ButtonListener buttonListener;
-	
-	private final String trianglePath = "/Images/triangle.jpg";
+
+	private final String mPath = "/Images/minesM.jpg";
 	private final String bannerPath = "/Images/banner.jpg";
-	
-	private BufferedImage triangleLogo;
-	private BufferedImage banner;
-	
-	private JLabel triangleLabel;
+
+	private JLabel minesMlabel;
 	private JLabel bannerLabel;
-	
+
+	// For keeping track of which panel to go to when the "back" button is pressed
 	private static Stack<JPanel> panelStack;
-	
+
 	public MainGUI() {
-		
+
 		reader = new InputReader(this);
-		centerPanel = new SwipeCardPanel();
+		mainPanel = new SwipeCardPanel();
 		buttonListener = new ButtonListener();
 		setup();
-		
+
 		panelStack = new Stack<JPanel>();
-		
-		ImageIcon triangleIcon = createImageIcon(trianglePath);
-		triangleLabel = new JLabel(triangleIcon);
-		
+
+		ImageIcon mIcon = createImageIcon(mPath);
+		minesMlabel = new JLabel(mIcon);
+
 		ImageIcon bannerIcon = createImageIcon(bannerPath);
 		bannerLabel = new JLabel(bannerIcon);
-		
-		System.out.println(triangleLabel.toString());
-		
-		try {
-		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-		        if ("Nimbus".equals(info.getName())) {
-		            UIManager.setLookAndFeel(info.getClassName());
-		            break;
-		        }
-		    }
-		} catch (Exception e) {
-			// use default
-		}
-		
+
 		backButton = new JButton("Back");
 		backButton.addActionListener(buttonListener);
-		
+
 	}
 
-    protected static ImageIcon createImageIcon(String path) {
-    	
-        java.net.URL imageURL = MainGUI.class.getResource(path);
+	protected static ImageIcon createImageIcon(String path) {
 
-        if (imageURL == null) {
-            System.err.println("Resource not found: " + path);
-            return null;
-        } else {
-            return new ImageIcon(imageURL);
-        }
-    }
-	
+		java.net.URL imageURL = MainGUI.class.getResource(path);
+
+		if (imageURL == null) {
+			System.err.println("Resource not found: " + path);
+			return null;
+		} else {
+			return new ImageIcon(imageURL);
+		}
+	}
+
 	public void setup() {
 		//sets the cursor to invisible
-//		tk = Toolkit.getDefaultToolkit();
-//		BufferedImage image = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
-//		setCursor(tk.createCustomCursor(image, new Point(0,0), "blank"));
+		//		tk = Toolkit.getDefaultToolkit();
+		//		BufferedImage image = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+		//		setCursor(tk.createCustomCursor(image, new Point(0,0), "blank"));
 
 		//sets the screen to full screen
 		setExtendedState(MAXIMIZED_BOTH);
 		setUndecorated(true);
 		setResizable(false);
-		
+
 		setLayout(new GridBagLayout());
-		
+
 		//disable Alt+F4
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		add(centerPanel);
+		add(mainPanel);
 		setVisible(true);
 
 		addKeyListener(reader);
 		setFocusable(true);
 
 		tracker = Driver.getAccessTracker();
+
+		// Set up the look and feel of the frame/panels
+		try {
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (Exception e) {
+			// use default look and feel
+		}
 	}
 
 	public enum SearchBy {
@@ -134,15 +136,17 @@ public class MainGUI extends JFrame {
 		}
 	};
 	
+	// Returns the user to the "Swipe Blastercard" screen
 	public void restart() {
+		
 		Driver.isLogInScreen = true;
-		remove(headerBar);
-		remove(homeCenterPanel);
-		add(centerPanel);
+		this.removeAll();
+		add(mainPanel);
 		setVisible(true);
 		repaint();
+		
 	}
-	
+
 	public void handleInput() {
 
 		InputReader inReader = (InputReader) reader;
@@ -155,16 +159,16 @@ public class MainGUI extends JFrame {
 			if (InputReader.isValidCWID(inReader.getCWID())) {
 				login(inReader.getCWID());
 			}
-			
+
 		} catch (InputReaderException e) {
-			
+
 			if (InputReader.getErrorCount() < MAX_ERROR_COUNT) {
 				JOptionPane.showMessageDialog(this, e.getMessage());
 				return;
 			}
-			
+
 			String input;
-			
+
 			if (InputReader.getErrorCount() == 999) {
 				input = JOptionPane.showInputDialog("Enter CWID.");
 			} else {
@@ -174,7 +178,7 @@ public class MainGUI extends JFrame {
 				InputReader.resetErrorCount();
 				return;
 			}
-			
+
 			int tries = 0;
 			while (!InputReader.isValidCWID(input) && tries < MAX_ERROR_COUNT) {
 				input = JOptionPane.showInputDialog("Not a valid CWID. Please try again. ");
@@ -184,22 +188,22 @@ public class MainGUI extends JFrame {
 				}
 				++tries;
 			}
-			
+
 			InputReader.resetErrorCount();
 			if (InputReader.isValidCWID(input)){
 				login(input);
 			}
 		}
 	}
-	
+
 	public void login(String CWID){
 		currentUser = tracker.processLogIn(CWID);
 		if ( currentUser != null ) {
-			remove(centerPanel);
+			remove(mainPanel);
 			processHomeScreen(currentUser);
 		}
 	}
-	
+
 	public void enterPressed() {
 		handleInput();
 	}
@@ -208,92 +212,97 @@ public class MainGUI extends JFrame {
 		return currentUser;
 	}
 	
+	// This function does most of the work to set up the homescreen for whichever
+	// user is currently logged in (whether they are an Admin, SystemAdmin, or User)
+	
 	public void processHomeScreen(User currentUser) {
-		
+
 		headerFont = new Font("SansSerif", Font.BOLD, 42);
 		headerBar = new JPanel(new GridLayout(1, 2));
 		headerBar.setBackground(Color.white);
-		
+
 		footerBar = new JPanel(new GridBagLayout());
 		footerBar.setBackground(Color.white);
 		footerBar.add(bannerLabel);
-		
+
 		String userName = currentUser.getFirstName() + " " + currentUser.getLastName();
-		
+
 		JLabel nameLabel = new JLabel(userName);
 		nameLabel.setBackground(Color.white);
 		time = new Clock(headerFont);
-		
+
 		nameLabel.setHorizontalAlignment(JLabel.LEFT);
 		time.setHorizontalAlignment(JLabel.RIGHT);
-		
+
 		nameLabel.setFont(headerFont);
-		
+
 		JPanel leftPanel = new JPanel(new GridBagLayout());
 		leftPanel.setBackground(Color.white);
-		
+
 		c.gridx = 0;
 		c.gridy = 0;
-		c.weightx = 0.2;
+		c.weightx = 0.15;
 		c.weighty = 1;
 		c.fill = GridBagConstraints.NONE;
-		leftPanel.add(triangleLabel, c);
-		
-		c.gridx = 2;
-		c.weightx = 0.8;
+		leftPanel.add(minesMlabel, c);
+
+		c.gridx = 1;
+		c.gridy = 0;
+		c.weightx = 0.85;
 		c.fill = GridBagConstraints.HORIZONTAL;
 		leftPanel.add(nameLabel, c);
-		
+
 		headerBar.add(leftPanel);
 		headerBar.add(time);
 
 		c.gridx = 0;
 		c.gridy = 0;
-		c.weightx = 0.1;
-		c.weighty = 0;
-		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;
+		c.weighty = 0.05;
+		c.fill = GridBagConstraints.BOTH;
 		add(headerBar, c);
-		
-		homeCenterPanel = new JPanel();
-		
+
+		mainContentPanel = new JPanel();
+		mainContentPanel.setBackground(Color.white);
+
 		if ( currentUser.isAdmin() ) {
 			if ( ((Administrator) currentUser).isSystemAdmin() ) {
-				homeCenterPanel = new SystemAdminGUI(currentUser);
+				mainContentPanel = new SystemAdminGUI(currentUser);
 			} else {
-				homeCenterPanel = new AdminGUI(currentUser);
-		}
+				mainContentPanel = new AdminGUI(currentUser);
+			}
 		} 	else {
-			homeCenterPanel = new UserGUI(currentUser);
+			mainContentPanel = new UserGUI(currentUser);
 		}
-		
+
+		c.gridx = 0;
 		c.gridy = 1;
-		c.gridx = 0;
-		c.weighty = 0.8;
+		c.weighty = 0.9;
 		c.fill = GridBagConstraints.BOTH;
-		add(homeCenterPanel, c);
-		
-		c.gridy = 2;
+		add(mainContentPanel, c);
+
 		c.gridx = 0;
-		c.weighty = 0.8;
-		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridy = 2;
+		c.weighty = 0.05;
+		c.fill = GridBagConstraints.BOTH;
 		add(footerBar, c);
-		
+
 		setVisible(true);
-		
+
 	}
-	
+
 	public void goBack() {
 		System.out.println(panelStack.toString());
 		if ( !panelStack.isEmpty() )
 			panelStack.pop();
 		if ( !panelStack.isEmpty() )
-			homeCenterPanel = panelStack.pop();
+			mainContentPanel = panelStack.pop();
 	}
-	
+
 	public static void pushToStack(JPanel panel) {
 		panelStack.push(panel);
 	}
-	
+
 	private class ButtonListener implements ActionListener {
 
 		@Override
@@ -303,7 +312,7 @@ public class MainGUI extends JFrame {
 				repaint();
 			}
 		}
-		
+
 	}
-	
+
 }
